d5505741b1a3658b47059aa0f058e21a8c520e45
Sayyam Jain <143936791+Sayyamjain23@users.noreply.github.com>
2025-04-10 11:17:10 +0530

    Update Banner.js

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -20,7 +20,7 @@ export const Banner = () => {
     }, delta);
 
     return () => { clearInterval(ticker) };
-  }, [text,delta])
+  }, [tick,delta])
 
   const tick = () => {
     let i = loopNum % toRotate.length;
