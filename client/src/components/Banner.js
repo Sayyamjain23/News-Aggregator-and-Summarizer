297ccb77b114ee8ec003d560d66cf29dcc4ab01e
Sayyam Jain <143936791+Sayyamjain23@users.noreply.github.com>
2025-04-10 11:31:11 +0530

    Update Banner.js

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -11,8 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-
-  const toRotate(()=> ["Aggregator", "Summariser", "Curator"],[]);
+  const toRotate = useMemo(()=>["Aggregator", "Summariser", "Curator"],[]);
   const period = 2000;
 
   useEffect(() => {
